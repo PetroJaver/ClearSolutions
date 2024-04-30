a88a2e04186e3f26e8091e8839be339023efc9ff
Peter, Slusarenko <petslu23@gmail.com>
2024-04-30 19:16:53 +0300

    init

---
+++ src/test/java/org/clearsolutions/restapi/ClearSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.clearsolutions.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
